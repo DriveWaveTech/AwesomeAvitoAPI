907a7bf20b67c5ab4a2a3c1fae6d4aefe9de3681
Irrenriel <yuronsuper@gmail.com>
2024-10-07 11:00:48 +0300

    Setup fix

--- AwesomeAvitoAPI/__init__.py
+++ AwesomeAvitoAPI/__init__.py
@@ -1,1 +1,20 @@
-from .AwesomeAvitoAPI import AwesomeAvitoAPI
+__all__ = (
+    'AwesomeAvitoAPI',
+)
+
+from modules.AvitoAdvertisements import AvitoAdvertisements
+from modules.AvitoProfile import AvitoProfile
+from modules.AvitoToken import AvitoToken
+
+
+class AwesomeAvitoAPI(
+    AvitoToken,
+    AvitoProfile,
+    AvitoAdvertisements
+):
+    def __init__(
+        self,
+        *args,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)

de6f850c9b9f2a0d87de974570ff9ed3fe21305e
Irrenriel <yuronsuper@gmail.com>
2024-10-07 11:06:03 +0300

    Setup fix

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="AwesomeAvitoAPI",
-    version="1.0",
-    packages=find_packages(),
+setuptools.setup(
+    include_package_data=True,
+    name='AwesomeAvitoAPI',
+    version='0.1',
+    description='Awesome async library for working with AvitoAPI!',
+    url='https://github.com/Irrenriel/AwesomeAvitoAPI',
+    author='Irrenriel',
+    packages=setuptools.find_packages(),
     install_requires=[
         'aiohttp>=3.10.9',
         'pydantic>=2.9.2',
         'loguru>=0.7.2',
         'python-dateutil>=2.9.0.post0',
     ],
-    include_package_data=True,
-    python_requires=">=3.11",
+    long_description='Awesome async library for working with AvitoAPI!',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
 )

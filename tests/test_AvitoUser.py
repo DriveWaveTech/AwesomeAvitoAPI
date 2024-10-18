1161d39af7df1d705a2e333830fd9444f524e699
Alisher <raindeero@mail.ru>
2024-10-18 16:59:15 +0500

    Avito user tests

--- tests/test_AvitoUser.py
+++ tests/test_AvitoUser.py
@@ -1,13 +1,25 @@
 import pytest
-
+from AwesomeAvitoAPI.responses import UserInfoSelfResponse, UserBalanceResponse
 
 @pytest.mark.asyncio
 class TestAvitoUser:
-    async def test_get_user_info_self():
-        ...
+    async def test_get_user_info_self(self, awesome_avito_api):
+        user_info = await awesome_avito_api.get_user_info_self()
+        assert isinstance(user_info, UserInfoSelfResponse)
 
-    async def test_get_user_balance():
-        ...
+        assert isinstance(user_info.id, int)
+        assert isinstance(user_info.email, str)
+        assert isinstance(user_info.name, str)
+        assert isinstance(user_info.phone, int)
+        assert isinstance(user_info.profile_url, str)
 
-    async def test_post_operations_history():
+    async def test_get_user_balance(self, awesome_avito_api):
+        user_balance = await awesome_avito_api.get_user_balance()
+        assert isinstance(user_balance, UserBalanceResponse)
+
+        assert isinstance(user_balance.bonus, float)
+        assert isinstance(user_balance.real, float)
+
+    @pytest.mark.skip(reason="Пропускаем этот тест")
+    async def test_post_operations_history(self, awesome_avito_api):
         ...
